7abfad2a07695d3dd0c8cdf0c2b6cbd3582d0208
scotttyso <tyscott@cisco.com>
2022-09-23 21:43:29 -0400

    pre-commit documentation updates

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,8 +2,7 @@ module "device_connector_policy" {
   source  = "terraform-cisco-modules/policies-device-connector/intersight"
   version = ">= 1.0.1"
 
-  description      = "default Device Connector Policy."
+  description  = "default Device Connector Policy."
   name         = "default"
   organization = "default"
 }
-
