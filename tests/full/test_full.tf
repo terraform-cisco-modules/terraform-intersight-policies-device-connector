b796c27c18f274ad256e10f5ebfe577f6451264c
scotttyso <tyscott@cisco.com>
2022-10-06 10:12:19 -0400

    moving terratest to org terratest

--- tests/full/test_full.tf
+++ tests/full/test_full.tf
@@ -31,5 +31,5 @@ module "main" {
   configuration_lockout = false
   description           = "${var.name} Device Connector Policy."
   name                  = var.name
-  organization          = "default"
+  organization          = "terratest"
 }
